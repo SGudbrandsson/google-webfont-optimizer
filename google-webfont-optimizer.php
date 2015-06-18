d941ef0e531334bd01ea1cdff969d4fff02e4ce0
Sigurður Guðbrandsson <sigurdur@sigginet.info>
2015-06-18 02:10:06 +0000

    Change version to 0.2.3

--- google-webfont-optimizer.php
+++ google-webfont-optimizer.php
@@ -3,7 +3,7 @@
  * Plugin Name: Google Webfont Optimizer
  * Plugin URI: http://quickfalcon.com/
  * Description: This plugin optimizes the way Google Fonts loads on your webpage, increasing your website's performance.
- * Version: 0.2.2
+ * Version: 0.2.3
  * Author: Sigurdur Gudbrandsson
  * Author URI: http://quickfalcon.com/
  * License: GPL2
@@ -29,7 +29,7 @@
 require dirname( __FILE__ ) . '/scb/load.php';
 
 class GWFO {
-    const version = '0.2.2';
+    const version = '0.2.3';
     protected static $_instance;
 
     // Class Construct
